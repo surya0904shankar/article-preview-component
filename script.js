3da072372715b2126f029893830377bf93767e60
codersurya0904 <89637956+surya0904shankar@users.noreply.github.com>
2025-09-03 19:23:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".share-icon-container").addEventListener("click", function() {
+document.querySelector(".triangle").style.display = "flex";
+document.querySelector(".share").style.display = "flex";
+});
